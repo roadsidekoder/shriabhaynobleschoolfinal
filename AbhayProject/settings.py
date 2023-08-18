b294a92a01d9cea6a83f796aa29b287299d9f427
c013ra <c013ra@c013ras-MacBook-Pro.local>
2023-08-18 18:59:25 +0530

    set default value for DEBUG

--- AbhayProject/settings.py
+++ AbhayProject/settings.py
@@ -31,7 +31,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,  'templates')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=False)
 
 
 ALLOWED_HOSTS = ['*']
