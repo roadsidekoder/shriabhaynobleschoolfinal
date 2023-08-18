670ec1fc438d11cafd3f0f9247999ba8493081f5
c013ra <c013ra@c013ras-MacBook-Pro.local>
2023-08-18 18:13:41 +0530

    small fix

--- AbhayProject/settings.py
+++ AbhayProject/settings.py
@@ -23,11 +23,13 @@ import environ
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,  'templates')
-environ.Env.read_env(BASE_DIR / '.env') 
 env = environ.Env(  # <-- Updated!
     # set casting, default value
     DEBUG=(bool, False),
 )
+
+environ.Env.read_env(BASE_DIR / '.env') 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
